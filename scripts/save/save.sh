2680f7ae8bd4816ea92f87cab653cbb843b3d9df
Lincoln Yan <yyjlincoln@gmail.com>
2022-06-12 16:01:20 +1000

    Updated save.sh

--- scripts/save/save.sh
+++ scripts/save/save.sh
@@ -1,6 +1,8 @@
-git status
+echo "Getting the latest changes..."
+git pull --rebase
 git add .
-echo "You will be saving the following changes:"
+git status
+echo "You will be saving the above changes:"
 echo "Please enter a commit message:"
 read commit_message
 if [ -z "$commit_message" ]; then
